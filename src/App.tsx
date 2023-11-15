42c6be5db50be159e585af64e17fb6406bd8c310
dosepehr <dosepehr@gmail.com>
2023-11-15 19:30:01 +0330

    add Hero component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Header from "./Components/common/Header";
+import Hero from "./Components/common/Hero";
 
 const App: React.FC = () => {
   return (
-    <div className="mx-auto sm:px-10 md:px-14 lg:px-28 py-2">
+    <div className="mx-auto py-2 sm:px-10 md:px-14 lg:px-28">
       <Header />
+      <Hero />
     </div>
   );
 };
---
+++ src/Components/common/Hero.tsx
@@ -0,0 +1,18 @@
+const Hero = () => {
+  return (
+    <>
+      <div className="flex flex-col">
+        <p>get your discount</p>
+        <p>flavored espresso</p>
+        <p className="max-w-[40%]">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+          maxime mollitia fuga, nulla eaque animi, delectus architecto commodi
+          suscipit doloribus quas error, tenetur voluptate repellat consequuntur
+          hic ipsum! Rerum, totam?
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
