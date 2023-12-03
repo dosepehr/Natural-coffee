eb9d1324055f7b1379e53285eec55d003ca270ff
dosepehr <dosepehr@gmail.com>
2023-12-03 23:34:51 +0330

    edit Navbar styles

--- src/Components/common/Navbar.tsx
+++ src/Components/common/Navbar.tsx
@@ -1,24 +1,37 @@
 import { Link } from "react-router-dom";
 const Navbar = () => {
-  const LinkStyles =
-    "text-base font-extrabold leading-6 text-mainBrown after:duration-300 after:block after:h-[2px] after:w-0 after:bg-mainBrown after:content-[''] hover:after:w-full";
   return (
     <div className="flex items-center">
       <img src="/images/Logo.png" alt="" />
       <div className="ml-5 mt-4 hidden w-full space-x-4 md:flex ">
-        <Link to="/" className={LinkStyles}>
+        <Link
+          to="/"
+          className="text-white text-base font-extrabold leading-6 lg:text-mainBrown after:block after:h-[2px] after:w-0 after:bg-mainBrown after:duration-300 after:content-[''] hover:after:w-full"
+        >
           HOME
         </Link>
-        <Link to="/shop" className={LinkStyles}>
+        <Link
+          to="/shop"
+          className="text-white text-base font-extrabold leading-6 lg:text-mainBrown after:block after:h-[2px] after:w-0 after:bg-mainBrown after:duration-300 after:content-[''] hover:after:w-full"
+        >
           SHOP
         </Link>
-        <Link to="/about-us" className={LinkStyles}>
+        <Link
+          to="/about-us"
+          className="text-white text-base font-extrabold leading-6 lg:text-mainBrown after:block after:h-[2px] after:w-0 after:bg-mainBrown after:duration-300 after:content-[''] hover:after:w-full"
+        >
           ABOUT US
         </Link>
-        <Link to="/blog" className={LinkStyles}>
+        <Link
+          to="/blog"
+          className="text-white text-base font-extrabold leading-6 lg:text-mainBrown after:block after:h-[2px] after:w-0 after:bg-mainBrown after:duration-300 after:content-[''] hover:after:w-full"
+        >
           OUR BLOG
         </Link>
-        <Link to="/contact-us" className={LinkStyles}>
+        <Link
+          to="/contact-us"
+          className="text-white text-base font-extrabold leading-6 lg:text-mainBrown after:block after:h-[2px] after:w-0 after:bg-mainBrown after:duration-300 after:content-[''] hover:after:w-full"
+        >
           CONTACT US
         </Link>
       </div>
