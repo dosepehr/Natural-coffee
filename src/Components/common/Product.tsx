cbab483e8eb5154ea74bd1133c343e7423d98b66
dosepehr <dosepehr@gmail.com>
2023-12-08 02:14:34 +0330

    add Product cpmponent

--- src/Components/common/Product.tsx
+++ src/Components/common/Product.tsx
@@ -13,48 +13,55 @@ const Product = () => {
   const { data: product, isLoading } = useGetProductQuery(Number(id));
   return (
     <>
-      <div className="bg-[#F2ECEC]">
+      <div className="mx-auto bg-[#F2ECEC]">
         {isLoading ? (
           <p>loading</p>
         ) : (
-          <div className="mx-auto flex max-w-6xl flex-row items-center justify-center py-10">
-            <div>
+          <div className="mx-auto flex h-[700px] max-w-6xl flex-col items-center justify-center py-10 md:flex-row">
+            <div className="order-1 h-full w-full md:order-[0]">
               <img
                 src={
                   `http://localhost:5000/images/${product?.image}` ||
                   "/images/product.png"
                 }
+                className="h-full object-contain"
                 alt=""
               />
             </div>
-            <div className="mx-auto ml-5 space-y-4">
-              <p className="text-5xl font-extrabold">{product?.name}</p>
-              <p className="text-2xl text-secondaryBrown ">${product?.price}</p>
-              <p className="max-w-[60%] text-base text-secondaryGray ">
-                {product?.description}
-              </p>
-              <div className="justify-s flex items-center space-x-4">
-                <Button type="count">
-                  <LuPlus />
-                </Button>
-                <p>1</p>
-                <Button type="count">
-                  <LuMinus />
-                </Button>
-                <Button className="!flex items-center space-x-2 !px-8">
-                  <span className="!text-2xl">
-                    <CiShoppingBasket />
-                  </span>
-                  <span className="text-sm">ADD TO CARD</span>
-                </Button>
-                <Button type="count" className="!p-3">
-                  <Heart />
-                </Button>
-              </div>
-              <div className="flex items-center justify-start">
-                <p className="mr-4">Share:</p>
-                <div className="flex space-x-3 text-base text-black">
-                  <Socials />
+            <div className="">
+              <div className="ml-5 flex flex-col justify-center">
+                <div className="space-y-4">
+                  <p className="text-5xl font-extrabold">{product?.name}</p>
+                  <p className="text-2xl text-secondaryBrown ">
+                    ${product?.price}
+                  </p>
+                  <p className="max-w-[60%] text-base text-secondaryGray ">
+                    {product?.description}
+                  </p>
+                </div>
+                <div className="mb-4 flex items-center space-x-4">
+                  <Button type="count">
+                    <LuPlus />
+                  </Button>
+                  <p>1</p>
+                  <Button type="count">
+                    <LuMinus />
+                  </Button>
+                  <Button className="!flex items-center space-x-2 !px-8">
+                    <span className="!text-2xl">
+                      <CiShoppingBasket />
+                    </span>
+                    <span className="text-sm">ADD TO CARD</span>
+                  </Button>
+                  <Button type="count" className="!p-3">
+                    <Heart />
+                  </Button>
+                </div>
+                <div className="flex items-center justify-start ">
+                  <p className="mr-4">Share:</p>
+                  <div className="flex space-x-3 text-base text-black">
+                    <Socials />
+                  </div>
                 </div>
               </div>
             </div>
